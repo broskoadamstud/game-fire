7a724f2b8913bd8650259376d34ab307646cd0b9
Adam Broko <brosko@stud.uniza.sk>
2022-11-07 17:34:27 +0100

    skuska

---
+++ game-fire/casovac.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class casovac here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class casovac {
+    /**
+     * Constructor for objects of class casovac
+     */
+    public casovac() {
+        // initialise instance variables
+    }
+}
---
+++ game-fire/ohen.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class ohen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ohen {
+    /**
+     * Constructor for objects of class ohen
+     */
+    public ohen() {
+        // initialise instance variables
+    }
+}
